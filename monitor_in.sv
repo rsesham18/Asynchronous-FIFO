77fa34747399b1ba886534a1d06fdd7371d0db38
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-05-03 22:50:39 -0700

    Update monitor_in.sv

--- monitor_in.sv
+++ monitor_in.sv
@@ -1,49 +1,41 @@
-class driver;
-  int tx_count1=0;
-  int tx_count2=0;
-  
-  virtual async_fifo_if intf_vi;
-  mailbox gen2driv;
- 
-  function new(virtual async_fifo_if intf_vi, mailbox gen2driv);
-    this.intf_vi = intf_vi;
-    this.gen2driv = gen2driv;
+
+//`include "transaction.sv"
+
+class monitor_in;
+  virtual async_fifo_if vif;
+  mailbox mon_in2scb;
+  transaction tx=new();
+int fd;
+  function new(virtual async_fifo_if vif, mailbox mon_in2scb);
+    this.vif = vif;
+    this.mon_in2scb = mon_in2scb;
   endfunction
   
-  task reset;
-    intf_vi.wr_rstn <= 0;
-    intf_vi.rd_rstn <= 0;
-    wait(!intf_vi.wr_rstn);
-    $display("[ DRIVER ] ****** RESET STARTED ******");
-    intf_vi.wr_inc <= 0;
-    intf_vi.wr_data <= 0;
-    intf_vi.rd_inc <= 0;
-    intf_vi.rd_data <= 0;
-    repeat(2) @(posedge intf_vi.wr_clk);
-    intf_vi.wr_rstn <= 1'b1;
-    intf_vi.rd_rstn <= 1'b1;
-    wait(intf_vi.wr_rstn);
-    $display("[ DRIVER ] ****** RESET ENDED ******");
-  endtask
-  
   task main;
-    $display("[ DRIVER ] ****** DRIVER STARTED ******");
-    
-    forever begin
-      transaction tx;  
-      gen2driv.get(tx);
-      intf_vi.uniq_id = tx.uniq_id;
-      
-      @(posedge intf_vi.wr_clk);
-      tx_count1++;
-      intf_vi.uniq_id =tx.uniq_id;
-      intf_vi.wr_data = tx.wr_data;
-      if(intf_vi.uniq_id ==1)
-        $display("[ DRIVER ] wdata =%h uniq_id =%d \n",intf_vi.wr_data,intf_vi.uniq_id);
      
-      if (intf_vi.wr_inc== 1'b1) tx_count2++;
-    end
+    $display("[ MONITOR_IN ] ****** MONITOR_IN STARTED ******"); 
+   
+     
+            
+ 
+    forever begin
+       
+      @(posedge vif.wr_clk iff !vif.wr_full);
+        
+      if (vif.wr_inc) begin
+           
+           tx.wr_data =vif.wr_data;
+           tx.uniq_id =vif.uniq_id;
+             if(vif.uniq_id ==1)
+               $display("[ MONITOR]wdata =%h,uniq_id =%d \n",tx.wr_data,tx.uniq_id);
+          mon_in2scb.put(tx);
+         
+          fd= $fopen("output1.txt","a");
+            
+     // $fdisplay(fd,"monitor_in wdata  =%h",tx.wdata);
+        end
+     end 
     
-    $display("[ DRIVER ] ****** DRIVER ENDED ******");
+    $display("[ MONITOR_IN ] ****** MONITOR_IN ENDED ******");    
   endtask
 endclass
