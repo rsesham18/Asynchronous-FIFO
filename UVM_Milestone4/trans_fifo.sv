88fbcd42a02031089e12ed1cf5d91434954117b0
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 11:02:04 -0700

    Update trans_fifo.sv

--- UVM_Milestone4/trans_fifo.sv
+++ UVM_Milestone4/trans_fifo.sv
@@ -1,14 +1,14 @@
 class trans_fifo;
 
-    parameter  data_width =8, ptr_width = 8;
+    parameter  Data_Width =8, Addr_Width = 8;
 	rand bit w_en;
 	rand bit r_en;
-	randc bit [data_width-1:0] data_in;
+	randc bit [Data_Width-1:0] data_in;
 	int flag,mflag;
-	bit [data_width-1:0] data_out;
+	bit [Data_Width-1:0] data_out;
 	bit empty, full;
-	bit [ptr_width:0] waddr;
-	bit [ptr_width:0]raddr;
+	bit [Addr_Width:0] waddr;
+	bit [Addr_Width:0] raddr;
 	
 endclass
 	
