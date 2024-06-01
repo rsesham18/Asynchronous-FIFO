47d3469377ec56f4afc8b2d9f119cdf373528144
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 15:48:07 -0700

    Update and rename read_pointer.sv to read_ptr.sv

--- UVM_Milestone4/rtl/read_ptr.sv
+++ UVM_Milestone4/rtl/read_ptr.sv
@@ -6,12 +6,12 @@ module read_ptr #(parameter Addr_Width=8)( rd_clk, rd_rstn, rd_en,   wr_ptr_sync
 	output logic [Addr_Width:0] rd_addr, rd_ptr;
 
  	logic rd_empty;
-
+	
 	logic [Addr_Width:0] rd_addr_next;
 	logic [Addr_Width:0] rd_ptr_next;
 
 
-assign raddr_next= rd_addr + (rd_en & !empty);
+	assign rd_addr_next= rd_addr + (rd_en & !empty);
 	assign rd_ptr_next=(rd_addr_next>>1)^rd_addr_next; // GRAY CONVERTED VALUE
 	assign rd_empty= (wr_ptr_sync == rd_ptr_next); // CHECKING THE EMPTY CONDITION 
 
