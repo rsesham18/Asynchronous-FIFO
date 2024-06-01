0f7f69cbd4987ea9af95fa6b4dea8f3cbd6a0667
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 10:59:11 -0700

    Update write_pointer.sv

--- UVM_Milestone4/write_pointer.sv
+++ UVM_Milestone4/write_pointer.sv
@@ -1,14 +1,14 @@
-module write_ptr #(parameter ptr_width=8)(wclk, w_rst_n, w_en,  rptr_sync,  waddr, wptr, full);
+module write_ptr #(parameter Addr_Width=8)(wclk, w_rst_n, w_en,  rptr_sync,  waddr, wptr, full);
 
 input bit wclk, w_rst_n, w_en;
-input logic [ptr_width:0] rptr_sync;
+	input logic [Addr_Width:0] rptr_sync;
 
 output bit full;
-output logic [ptr_width:0] waddr, wptr;
+	output logic [Addr_Width:0] waddr, wptr;
 
 logic wfull;
-logic [ptr_width:0]waddr_next; 
-logic [ptr_width:0]wptr_next;
+	logic [Addr_Width:0]waddr_next; 
+	logic [Addr_Width:0]wptr_next;
 
 assign waddr_next= waddr + (w_en & !full);
 assign wptr_next= (waddr_next>>1)^waddr_next; 
