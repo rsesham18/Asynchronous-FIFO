2d754e294fab06af5a8067756188057656d6c610
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 10:54:15 -0700

    Update fifo_mem.sv

--- UVM_Milestone4/fifo_mem.sv
+++ UVM_Milestone4/fifo_mem.sv
@@ -1,19 +1,19 @@
-module fifo_mem #(parameter data_width =  8, ptr_width =  8, depth =256)( wclk,rclk,r_rst_n,w_rst_n,w_en,r_en,full,empty, data_in,  waddr,raddr,  data_out);
+module fifo_mem #(parameter Data_Width =  8, Addr_Width =  8, Depth =256)( wclk,rclk,r_rst_n,w_rst_n,w_en,r_en,full,empty, data_in,  waddr,raddr,  data_out);
 
 input bit wclk,rclk,r_rst_n,w_rst_n,w_en,r_en,full,empty;
-input logic [ptr_width:0]  waddr, raddr;
+  input logic [Addr_Width:0]  waddr, raddr;
 
-input logic [data_width-1:0] data_in;
-output logic [data_width-1:0]data_out;
+  input logic [Data_Width-1:0] data_in;
+  output logic [Data_Width-1:0]data_out;
 
-logic [data_width-1:0] fifo [0: depth-1];
+  logic [Data_Width-1:0] fifo [0: Depth-1];
 
 always_ff@(posedge wclk)
 begin
 if(w_en & !full)
 begin
-fifo[waddr[ptr_width-1:0]]<=data_in; // WRITE OPERATION
+  fifo[waddr[Addr_width-1:0]]<=data_in; // WRITE OPERATION
 end
 end
-assign data_out=fifo[raddr[ptr_width-1:0]]; // READ OPERATION
+  assign data_out=fifo[raddr[Addr_width-1:0]]; // READ OPERATION
 endmodule
