e99996cd4dc53282a092cef79ed5aa4d88e3792c
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 15:31:03 -0700

    Update read2writesync.sv

--- UVM_Milestone4/rtl/read2writesync.sv
+++ UVM_Milestone4/rtl/read2writesync.sv
@@ -1,18 +1,18 @@
-module r2wsync #(parameter Addr_Width=8)( wrclk, wr_rst_n, rptr,  rptr_sync);
+module rd_2_wr_sync #(parameter Addr_Width=8)( wr_clk, wr_rstn, rd_ptr,  rd_ptr_sync);
 
-input bit wrclk, wr_rst_n;
-  input logic [Addr_Width:0]  rptr;
-  output logic [Addr_Width:0]rptr_sync;
+input bit wr_clk, wr_rstn;
+  input logic [Addr_Width:0]  rd_ptr;
+  output logic [Addr_Width:0] rd_ptr_sync;
 
-  logic [Addr_width:0] q1;
-  always_ff@(posedge wrclk) begin
-    if(!wr_rst_n) begin
+  logic [Addr_Width:0] q1;
+  always_ff@(posedge wr_clk) begin
+    if(!wr_rstn) begin
       q1 <= 0;
-      rptr_sync <= 0;//one clock cycle delay
+      rd_ptr_sync <= 0;//one clock cycle delay
     end
     else begin
-      q1 <= rptr;
-      rptr_sync <= q1;//two clock cycles delay
+      q1 <= rd_ptr;
+      rd_ptr_sync <= q1;//two clock cycles delay
     end
   end
 endmodule
