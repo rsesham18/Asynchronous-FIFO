b9d0a9336869aaa948141002acbe20f19bda904a
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 16:00:22 -0700

    Update and rename UVM_Milestone4/sim/sequence_fifo_write.sv to UVM_Milestone4/fifo_sequence.sv

--- UVM_Milestone4/fifo_sequence.sv
+++ UVM_Milestone4/fifo_sequence.sv
@@ -1,10 +1,11 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
+
 class fifo_sequence extends uvm_sequence;
 	
 	`uvm_object_utils (fifo_sequence) // registering the class with factory
 	
-	fifo_seq_item fifo_pkt; // creating the class handle for sequence item class
+	fifo_seq_item trans; // creating the class handle for sequence item class
 	
 	// creating a new constructor for write_sequence class
 	function new( string name = "fifo_sequence");
@@ -18,14 +19,14 @@ class fifo_sequence extends uvm_sequence;
 	task body();
 		begin
 		    `uvm_info("FIFO_SEQUENCE","INSIDE THE TASK BODY!",UVM_LOW)
-			fifo_pkt = fifo_seq_item#(8,8,256)::type_id ::create ("fifo_pkt");
-			start_item(fifo_pkt);
-			fifo_pkt.no_rst.constraint_mode(0);
-			assert (fifo_pkt.randomize() with {fifo_pkt.w_rst_n==0;fifo_pkt.r_rst_n==0;});
-			`uvm_info("SEQ",$sformatf("Generate new item: %s",fifo_pkt.convert2str()),UVM_LOW)
+			trans = fifo_seq_item#(8,8,256)::type_id ::create ("trans");
+			start_item(trans);
+			trans.no_rst.constraint_mode(0);
+			assert (trans.randomize() with {trans.wr_rstn==0;trans.rd_rstn==0;});
+			`uvm_info("SEQ",$sformatf("Generate new item: %s",trans.convert2str()),UVM_LOW)
 			$display("-----------------------In reset body--------------------------------------");
 			$display("------------------------------------------------------------------");
-			finish_item(fifo_pkt);
+			finish_item(trans);
 	    end
 	endtask
 	
@@ -36,7 +37,7 @@ class fifo_sequence_wr extends uvm_sequence;
 	
 	`uvm_object_utils (fifo_sequence_wr) // registering the class with factory
 	
-	fifo_seq_item fifo_pkt_wr; // creating the class handle for sequence item class
+	fifo_seq_item trans_wr; // creating the class handle for sequence item class
 	
 	// creating a new constructor for write_sequence class
 	function new( string name = "fifo_sequence_wr");
@@ -45,21 +46,21 @@ class fifo_sequence_wr extends uvm_sequence;
 	endfunction
 	 
 	 
-	int num=256;
+	int tx_count=256;
 	// randomizing with write_enable-1 and read enable-0
 	task body();
-		for (int i=0; i<num;i++) begin
+		for (int i=0; i<tx_count;i++) begin
 		    `uvm_info("FIFO_SEQUENCE","INSIDE THE TASK BODY!",UVM_LOW)
-			fifo_pkt_wr = fifo_seq_item#(8,8,256)::type_id ::create ("fifo_pkt_wr");
-			start_item(fifo_pkt_wr);
-			assert (fifo_pkt_wr.randomize() with {w_en==1 ; r_en==0;});
-			`uvm_info("SEQ",$sformatf("Generate new item: %s",fifo_pkt_wr.convert2str()),UVM_LOW)
+			trans_wr = fifo_seq_item#(8,8,256)::type_id ::create ("trans_wr");
+			start_item(trans_wr);
+			assert (trans_wr.randomize() with {wr_en==1 ; rd_en==0;});
+			`uvm_info("SEQ",$sformatf("Generate new item: %s",trans_wr.convert2str()),UVM_LOW)
 			$display("------------------------------------------------------------------");
 			$display("------------------------------------------------------------------");
-			finish_item(fifo_pkt_wr);
+			finish_item(trans_wr);
 			`uvm_info("SEQ",$sformatf(" Done Generate new item: %d",i),UVM_LOW)
 	    end
-		 `uvm_info("SEQ",$sformatf(" Done Generation of items: %d",num),UVM_LOW)
+		 `uvm_info("SEQ",$sformatf(" Done Generation of items: %d",tx_count),UVM_LOW)
 	endtask
 	
 endclass
@@ -69,7 +70,7 @@ class sequence_fifo_rd extends uvm_sequence;
 	
 	`uvm_object_utils (sequence_fifo_rd) // registered the read sequence class with factory
 	
-	fifo_seq_item fifo_rd_pkt;// class handle for sequence item 
+	fifo_seq_item trans_rd;// class handle for sequence item 
 	
 	// created a new constructor for the sequence_fifo_rd
 	function new( string name = "sequence_fifo_rd");
@@ -77,22 +78,22 @@ class sequence_fifo_rd extends uvm_sequence;
 		`uvm_info("FIFO_READ_SEQUENCE", "Inside Constructor!", UVM_LOW)
 	endfunction
 	 
-    int num=256;
+    int tx_count=256;
 	// randomizing with write_enable-0 and read enable-1
 	task body();
-		for (int i=0; i<num;i++) begin
+		for (int i=0; i<tx_count;i++) begin
 		    `uvm_info("FIFO_READ_SEQUENCE","INSIDE THE TASK BODY!",UVM_LOW)
-			fifo_rd_pkt = fifo_seq_item#(8,8,256)::type_id::create ("fifo_rd_pkt");
-			start_item(fifo_rd_pkt);
-			assert (fifo_rd_pkt.randomize() with {w_en==0 & r_en==1;});
-			`uvm_info("SEQ",$sformatf("Generate new item:%s ",fifo_rd_pkt.convert2str()),UVM_LOW)
+			fifo_rd_pkt = fifo_seq_item#(8,8,256)::type_id::create ("trans_rd");
+			start_item(trans_rd);
+			assert (trans_rd.randomize() with {wr_en==0 & rd_en==1;});
+			`uvm_info("SEQ",$sformatf("Generate new item:%s ",trans_rd.convert2str()),UVM_LOW)
 			
 			$display("------------------------------------------------------------------");
 			$display("------------------------------------------------------------------");
-			finish_item(fifo_rd_pkt);
+			finish_item(trans_rd);
 			`uvm_info("SEQ",$sformatf(" done Generate new item: %d",i),UVM_LOW)
 		end
-		 `uvm_info("SEQ",$sformatf(" Done Generation of items: %d",num),UVM_LOW)
+		 `uvm_info("SEQ",$sformatf(" Done Generation of items: %d",tx_count),UVM_LOW)
 	endtask
 	
 endclass	 
