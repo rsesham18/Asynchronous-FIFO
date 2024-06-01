a324826cdb22a4b1d5ee47fcbb9c8b1fcdd8647f
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 16:23:16 -0700

    Update scoreboard.sv

--- UVM_Milestone4/sim/scoreboard.sv
+++ UVM_Milestone4/sim/scoreboard.sv
@@ -1,8 +1,8 @@
-class scoreboard extends uvm_test;
+class scoreboard extends uvm_scoreboard;
 	`uvm_component_utils(scoreboard)
 	 bit [7:0]transaction_queue[$];
 	 fifo_seq_item trans[$];
-	 uvm_analysis_imp #(fifo_seq_item,scoreboard) scoreboard_port; 
+	uvm_analysis_imp #(fifo_seq_item,scoreboard) ap_port; 
 	 function new(string name= "scoreboard",uvm_component parent);
 		super.new(name,parent);
 		`uvm_info("Scoreboard", "Sbd",UVM_LOW)
@@ -12,7 +12,7 @@ class scoreboard extends uvm_test;
 	function void build_phase(uvm_phase phase);
 	 super.build_phase(phase);
 		`uvm_info("Scoreboard", "Build phase",UVM_LOW)
-		scoreboard_port = new("scoreboard_port",this);
+		ap_port = new("ap_port",this);
 		endfunction
 		
 	//Connect Phase
@@ -39,10 +39,10 @@ class scoreboard extends uvm_test;
 	 //write function for reference fifo queue
 	 function void write(fifo_seq_item seqitem);
 	 trans.push_front(seqitem);
-	   if(seqitem.w_en &!seqitem.full)
+	   if(seqitem.wr_en &!seqitem.full)
 	   begin
 		transaction_queue.push_front(seqitem.data_in);
-		`uvm_info("Scoreboard write data_in",$sformatf("Burtst Dtails:w_en=%d, data_in=%d, full=%0d",seqitem.w_en, seqitem.data_in, seqitem.full),UVM_LOW) 
+		`uvm_info("Scoreboard write data_in",$sformatf("Burtst Dtails:wr_en=%d, data_in=%d, full=%0d",seqitem.wr_en, seqitem.data_in, seqitem.full),UVM_LOW) 
 		end
 	endfunction
 	
@@ -52,11 +52,11 @@ class scoreboard extends uvm_test;
 	   bit [7:0] expected_data;
 	   bit [7:0] actual_data;
 	
-	   if(read_trans.r_en &!read_trans.empty)
+		if(read_trans.rd_en &!read_trans.empty)
 	   begin
 		actual_data = read_trans.data_out;
 	    expected_data= transaction_queue.pop_back();
-	  `uvm_info("Scoreboard_getting read data_in",$sformatf("Burtst Dtails:r_en=%d, data_in=%d, full=%0d",read_trans.r_en, read_trans.data_in, read_trans.empty),UVM_LOW)
+		   `uvm_info("Scoreboard_getting read data_in",$sformatf("Burtst Dtails:r_en=%d, data_in=%d, full=%0d",read_trans.rd_en, read_trans.data_in, read_trans.empty),UVM_LOW)
 		end
 		if(actual_data != expected_data) begin
 			`uvm_error("Comparing read", $sformatf("transaction failed actual_data=%d, expected_data=%d", actual_data,expected_data)) 
