d1180d7e5aeb53f3865780b7b483a7336229e235
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 11:06:18 -0700

    Update FIFO_sequence_items.sv

--- UVM_Milestone4/FIFO_sequence_items.sv
+++ UVM_Milestone4/FIFO_sequence_items.sv
@@ -1,16 +1,16 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-class fifo_seq_item #(parameter  data_width =8, ptr_width = 8, depth=256)extends uvm_sequence_item;
+class fifo_seq_item #(parameter  Data_Width =8, Addr_Width = 8, Depth=256)extends uvm_sequence_item;
     `uvm_object_utils(fifo_seq_item #(8,8,256))// registering 
 	rand bit w_en;
 	rand bit r_en;
 	rand bit w_rst_n;
 	rand bit r_rst_n;
-	rand bit [data_width-1:0] data_in;
-	bit [data_width-1:0] data_out;
+	rand bit [Data_Width-1:0] data_in;
+	bit [Data_Width-1:0] data_out;
 	bit empty, full;
-	bit [ptr_width:0] waddr, raddr;
+	bit [Addr_Width:0] waddr, raddr;
 	constraint no_rst {w_rst_n == 1 && r_rst_n ==1;}
 	
 	function string convert2str();
