5d1e35080ff86a6081ad32ed9af3b30c2114b94f
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 15:52:03 -0700

    Update FIFO_sequence_items.sv

--- UVM_Milestone4/sim/FIFO_sequence_items.sv
+++ UVM_Milestone4/sim/FIFO_sequence_items.sv
@@ -3,18 +3,18 @@ import uvm_pkg::*;
 
 class fifo_seq_item #(parameter  Data_Width =8, Addr_Width = 8, Depth=256)extends uvm_sequence_item;
     `uvm_object_utils(fifo_seq_item #(8,8,256))// registering 
-	rand bit w_en;
-	rand bit r_en;
-	rand bit w_rst_n;
-	rand bit r_rst_n;
+	rand bit wr_en;
+	rand bit rd_en;
+	rand bit wr_rstn;
+	rand bit rd_rstn;
 	rand bit [Data_Width-1:0] data_in;
 	bit [Data_Width-1:0] data_out;
 	bit empty, full;
-	bit [Addr_Width:0] waddr, raddr;
-	constraint no_rst {w_rst_n == 1 && r_rst_n ==1;}
+	bit [Addr_Width:0] wr_addr, rd_addr;
+	constraint no_rst {wr_rstn == 1 && rd_rstn ==1;}
 	
 	function string convert2str();
-	   return $sformatf ("w_en =%0d, r_en =%0d,data_in =%0d",w_en,r_en,data_in);
+		return $sformatf ("wr_en =%0d, rd_en =%0d,data_in =%0d",wr_en,rd_en,data_in);
 	endfunction
 	
 	// creating a new constructor 
