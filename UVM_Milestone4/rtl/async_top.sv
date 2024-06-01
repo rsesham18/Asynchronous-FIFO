e416219c337adfef6bcb629888d389dd48978a12
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 15:25:24 -0700

    Update async_top.sv

--- UVM_Milestone4/rtl/async_top.sv
+++ UVM_Milestone4/rtl/async_top.sv
@@ -1,17 +1,17 @@
-module top #(parameter Depth=256, Data_Width=8,Addr_Width=8) (intfc i1 );
+module async_top #(parameter Depth=256, Data_Width=8,Addr_Width=8) (intfc intf );
 
 
 //2 flop sunchronizers
-  w2rsync #(Addr_Width) w2rsync_inst(i1.rclk,  i1.r_rst_n,  i1.wptr ,  i1.wptr_sync );
-  r2wsync #(Addrr_Width) r2wsync_inst(i1.wclk,  i1.w_rst_n, i1.rptr,  i1.rptr_sync );
+  wr_2_rd_sync #(Addr_Width) w2rsync_inst(intf.rd_clk,  intf.rd_rstn,  intf.wr_ptr ,  intf.wr_ptr_sync );
+  rd_2_wr_sync #(Addrr_Width) r2wsync_inst(intf.wr_clk,  intf.wr_rstn, intf.rd_ptr,  intf.rd_ptr_sync );
 
 //write point handler
-  write_ptr #(Addr_Width) write_ptr_inst (i1.wclk, i1.w_rst_n, i1.w_en,  i1.rptr_sync,  i1.waddr, i1.wptr, i1.full);
+  write_ptr #(Addr_Width) write_ptr_inst (intf.wr_clk, intf.wr_rstn, intf.wr_en,  intf.rd_ptr_sync,  intf.wr_addr, intf.wr_ptr, intf.full);
 
 //read point handler
-  read_ptr #(Addr_Width) read_ptr_inst (i1.rclk, i1.r_rst_n, i1.r_en,   i1.wptr_sync,  i1.raddr, i1.rptr,  i1.empty);
+  read_ptr #(Addr_Width) read_ptr_inst (intf.rd_clk, intf.rd_rstn, intf.rd_en,   intf.wr_ptr_sync,  intf.rd_addr, intf.rd_ptr,  intf.empty);
 
 //fifo memory
-  fifo_mem #(Data_Width,ptr_width,depth) fifo_mem_inst (i1.wclk,i1.rclk,i1.r_rst_n,i1.w_rst_n,i1.w_en,i1.r_en,i1.full,i1.empty, i1.data_in,  i1.waddr,i1.raddr,  i1.data_out);
+  fifo_mem #(Data_Width,Addr_Width,Depth) fifo_mem_inst (intf.wr_clk,intf.rd_clk,intf.rd_rstn,intf.wr_rstn,intf.wr_en,intf.rd_en,intf.full,intf.empty, intf.data_in,  intf.wr_addr,intf.rd_addr,  intf.data_out);
 
 endmodule
