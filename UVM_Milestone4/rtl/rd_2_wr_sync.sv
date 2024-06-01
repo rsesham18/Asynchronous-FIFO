624e2ff8a6816f4ea96e9b6204c6334a6754b63c
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-01 15:35:10 -0700

    Update rd_2_wr_sync.sv

--- UVM_Milestone4/rtl/rd_2_wr_sync.sv
+++ UVM_Milestone4/rtl/rd_2_wr_sync.sv
@@ -4,15 +4,15 @@ input bit wr_clk, wr_rstn;
   input logic [Addr_Width:0]  rd_ptr;
   output logic [Addr_Width:0] rd_ptr_sync;
 
-  logic [Addr_Width:0] q1;
+  logic [Addr_Width:0] rd_ptr1;
   always_ff@(posedge wr_clk) begin
     if(!wr_rstn) begin
-      q1 <= 0;
+      rd_ptr1 <= 0;
       rd_ptr_sync <= 0;//one clock cycle delay
     end
     else begin
-      q1 <= rd_ptr;
-      rd_ptr_sync <= q1;//two clock cycles delay
+      rd_ptr1 <= rd_ptr;
+      rd_ptr_sync <= rd_ptr1;//two clock cycles delay
     end
   end
 endmodule
