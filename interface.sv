7d3a03cf28fb102d5b273f0471050b33f69d35f3
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-02 13:14:18 -0700

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,10 +1,10 @@
-interface async_fifo_if #(parameter DATA_SIZE = 8, ADDR_SIZE = 8);
-  logic [DATA_SIZE-1:0] wr_data;
-  logic wr_inc;
+/*interface async_fifo_if #(parameter DATA_SIZE = 8, ADDR_SIZE = 8);
+  logic [DATA_SIZE-1:0] data_in;
+  logic wr_en;
   logic wr_clk;
   logic wr_rstn;
-  logic [DATA_SIZE-1:0] rd_data;
-  logic rd_inc;
+  logic [DATA_SIZE-1:0] data_out;
+  logic rd_en;
   logic rd_clk;
   logic rd_rstn;
   logic wr_full;
@@ -18,5 +18,17 @@ interface async_fifo_if #(parameter DATA_SIZE = 8, ADDR_SIZE = 8);
     output rd_data, rd_inc, rd_empty
   );
 
-endinterface
+endinterface*/
+
+interface intfc(input bit wr_clk, rd_clk, wr_rstn, rd_rstn);
   
+parameter Depth=256, Data_Width=8, Addr_Width=8;
+  
+logic wr_en, rd_en;
+  
+logic [Addr_Width:0] rd_ptr_sync, wr_ptr_sync, wr_addr, wr_ptr,rd_addr, rd_ptr;
+bit full, empty;
+logic [Data_Width-1:0] data_in,data_out;
+//logic  [Data_Width-1:0] wr_data_q[$],rd_data;
+		
+endinterface 
