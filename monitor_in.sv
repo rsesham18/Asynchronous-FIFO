3fb37908204c63b0622e59b896e15f0883bcba9e
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-02 13:26:26 -0700

    Update monitor_in.sv

--- monitor_in.sv
+++ monitor_in.sv
@@ -1,12 +1,9 @@
-
-//`include "transaction.sv"
-
 class monitor_in;
-  virtual async_fifo_if vif;
+  virtual intfc vif;
   mailbox mon_in2scb;
   transaction tx=new();
 int fd;
-  function new(virtual async_fifo_if vif, mailbox mon_in2scb);
+  function new(virtual intfc vif, mailbox mon_in2scb);
     this.vif = vif;
     this.mon_in2scb = mon_in2scb;
   endfunction
@@ -14,25 +11,19 @@ int fd;
   task main;
      
     $display("[ MONITOR_IN ] ****** MONITOR_IN STARTED ******"); 
-   
-     
-            
  
     forever begin
        
-      @(posedge vif.wr_clk iff !vif.wr_full);
+      @(posedge vif.wr_clk iff !vif.full);
         
-      if (vif.wr_inc) begin
-           
-           tx.wr_data =vif.wr_data;
+      if (vif.wr_en) begin
+           tx.data_in =vif.data_in;
            tx.uniq_id =vif.uniq_id;
              if(vif.uniq_id ==1)
-               $display("[ MONITOR]wdata =%h,uniq_id =%d \n",tx.wr_data,tx.uniq_id);
+               $display("[ MONITOR]wdata =%h,uniq_id =%d \n",tx.data_in,tx.uniq_id);
           mon_in2scb.put(tx);
          
           fd= $fopen("output1.txt","a");
-            
-     // $fdisplay(fd,"monitor_in wdata  =%h",tx.wdata);
         end
      end 
     
