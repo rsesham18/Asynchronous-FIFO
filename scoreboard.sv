7e10ebc201224fc271feb5d4b775628c9a3a1379
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-02 13:39:56 -0700

    Update scoreboard.sv

--- scoreboard.sv
+++ scoreboard.sv
@@ -1,15 +1,15 @@
 class scoreboard;
   mailbox mon_in2scb;
   mailbox mon_out2scb;
-  virtual async_fifo_if vif;
+  virtual intfc vif;
   logic [7:0] rddata_fifo[$];
   logic [7:0] wrdata_fifo[$]; 
   int fd;
-logic [8-1:0] verif_data_q[$];
-int verif_data_id[$];
+  logic [8-1:0] verif_data_q[$];
+  int verif_data_id[$];
   logic [8-1:0] verif_wrdata;
   int verif_id;
-  function new(virtual async_fifo_if vif, mailbox mon_in2scb, mailbox mon_out2scb);
+ function new(virtual intfc vif, mailbox mon_in2scb, mailbox mon_out2scb);
     this.vif = vif;
     this.mon_in2scb  = mon_in2scb;
     this.mon_out2scb = mon_out2scb;
@@ -19,7 +19,6 @@ int verif_data_id[$];
 	fork 
       get_data_w();
       get_data_r();
-      //get_output();
     join_none;
   endtask
   
@@ -29,14 +28,14 @@ int verif_data_id[$];
    
    for(int i=0;i <100;i++) begin
       
-     @(posedge vif.wr_clk iff !vif.wr_full);
+     @(posedge vif.wr_clk iff !vif.full);
         
-     if (vif.wr_inc) begin
+     if (vif.wr_en) begin
           mon_in2scb.get(tx);
-       verif_data_q.push_front(tx.wr_data); 
+       verif_data_q.push_front(tx.data_in); 
            verif_data_id.push_front(tx.uniq_id); 
          if(tx.uniq_id==1)
-           $display("[SCOREBOARD] wdata =%h, uniq_id=%d",tx.wr_data,tx.uniq_id);
+           $display("[SCOREBOARD] wdata =%h, uniq_id=%d",tx.data_in,tx.uniq_id);
         end
      end 
     
@@ -46,22 +45,21 @@ int verif_data_id[$];
      transaction tx;
     
     fd= $fopen("output1.txt","w");
-     $fdisplay(fd,"Team1 scoreboard results");
+    $fdisplay(fd,"Team1 scoreboard results");
 
     for(int j=0;j<100;j++) begin
-      @(posedge vif.rd_clk iff !vif.rd_empty)
-        vif.rd_inc = (j%3 == 0)? 1'b1 : 1'b0;
-      if ((vif.rd_inc) &&(j!=0)) begin
+      @(posedge vif.rd_en iff !vif.empty)
+        vif.rd_en = (j%3 == 0)? 1'b1 : 1'b0;
+      if ((vif.rd_en) &&(j!=0)) begin
           verif_wrdata = verif_data_q.pop_back();
           verif_id = verif_data_id.pop_back();
               mon_out2scb.get(tx);
-        if(tx.rd_data === verif_wrdata) 
-          $display("PASS : expected wdata = %h, rdata = %h, uniq_id=%d", verif_wrdata, tx.rd_data,verif_id);
-	            else 
-                        $error("Checking failed: expected wdata = %h, rdata = %h,uniq_id=%d", verif_wrdata, tx.rd_data, verif_id);
-                    end
-        
-      end 
+        if(tx.data_out === verif_wrdata) 
+		$display("PASS : expected wr_ata = %h, rd_data = %h, uniq_id=%d", verif_wrdata, tx.data_out,verif_id);
+	else 
+		$error("Checking failed: expected wr_data = %h, rd_data = %h,uniq_id=%d", verif_wrdata, tx.data_out, verif_id);
+     end      
+  end 
       mon_out2scb.get(tx);
   endtask
 endclass
