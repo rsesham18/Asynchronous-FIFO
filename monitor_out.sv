207dfdd0a90d4d68cd0a2cd0ee96b66f79cd894c
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-02 13:31:51 -0700

    Update monitor_out.sv

--- monitor_out.sv
+++ monitor_out.sv
@@ -1,12 +1,12 @@
 //`include "transaction.sv"
 class monitor_out;
   int tx_count=0;
-  virtual async_fifo_if vif;
+  virtual intfc vif;
   mailbox mon_out2scb;
    transaction tx = new();
 int fd;
 int i;
-  function new(virtual async_fifo_if vif, mailbox mon_out2scb);
+  function new(virtual intfc vif, mailbox mon_out2scb);
     this.vif = vif;
     this.mon_out2scb = mon_out2scb;
   endfunction
@@ -16,20 +16,16 @@ int i;
   
     forever begin
         
-      @(posedge vif.rd_clk iff !vif.rd_empty)
-        vif.rd_inc = (i%3 == 0)? 1'b1 : 1'b0;
-      if ((vif.rd_inc)&&(i!=0)) begin
-             tx.rd_data =vif.rd_data;
-             
+      @(posedge vif.rd_clk iff !vif.empty)
+        vif.rd_en = (i%3 == 0)? 1'b1 : 1'b0;
+      if ((vif.rd_en)&&(i!=0)) begin
+             tx.data_out =vif.data_out;
              mon_out2scb.put(tx);
              tx_count++;
-             
              fd= $fopen("output1.txt","a");
-            // $fdisplay(fd,"monitor_out rdata  =%h",tx.rdata);
         end
   i++;
       end 
-   
     $display("[ MONITOR_OUT ] ****** MONITOR_OUT ENDED ******");    
   endtask
 endclass
