345106d1afee0bf945532fdafd2ff6232257f515
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-03 12:29:13 -0700

    Update interface.sv

--- SV/interface.sv
+++ SV/interface.sv
@@ -5,8 +5,7 @@ parameter Depth=256, Data_Width=8, Addr_Width=8;
 logic wr_en, rd_en;
 bit full, empty;
 logic [Data_Width-1:0] data_in, data_out;
-int uniq_id;
-  logic [Addr_Width:0] rd_ptr_sync, wr_ptr_sync, wr_addr, wr_ptr,rd_addr, rd_ptr;
+logic [Addr_Width:0] rd_ptr_sync, wr_ptr_sync, wr_addr, wr_ptr,rd_addr, rd_ptr;
 logic  [Data_Width-1:0] wdata_q[$],rdata;
 		
 /*//EMPTY Condition
