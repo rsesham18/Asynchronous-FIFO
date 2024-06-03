1957e42eef2fc114b3f303b2cb994ba1fab5492d
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-03 00:59:55 -0700

    Update interface.sv

--- SV/interface.sv
+++ SV/interface.sv
@@ -6,10 +6,10 @@ logic wr_en, rd_en;
 bit full, empty;
 logic [Data_Width-1:0] data_in, data_out;
 int uniq_id;
-logic [Addr_Width:0] rptr_sync, wptr_sync, waddr, wptr,raddr, rptr;
+  logic [Addr_Width:0] rd_ptr_sync, wr_ptr_sync, wr_addr, wr_ptr,rd_addr, rd_ptr;
 logic  [Data_Width-1:0] wdata_q[$],rdata;
 		
-//EMPTY Condition
+/*//EMPTY Condition
 assert property (@(posedge wr_clk or posedge rd_clk) disable iff (!(rd_rstn), !(wr_rstn))
     (wr_ptr == rd_ptr) |=> (empty == 1)
 ) else $fatal("FIFO empty flag error");
@@ -27,5 +27,5 @@ assert property (@(posedge wr_clk) disable iff (!(wr_rstn)
 // Read when FIFO is empty
  assert property (@(posedge rd_clk) disable iff (!(rd_rstn))
     !(rd_en && empty)
-) else $fatal("Read attempted when FIFO is empty");
+) else $fatal("Read attempted when FIFO is empty");*/
 endinterface 
