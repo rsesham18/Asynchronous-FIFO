832ae39d73f46ee3faf2f3053bc6f392bf71158a
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-05 18:18:54 -0700

    Update generator.sv

--- SV/generator.sv
+++ SV/generator.sv
@@ -1,26 +1,30 @@
 `include "transaction.sv"
 
 class generator;
-  rand transaction tx;
-  mailbox gen2driv;
-  virtual intfc bus_tb;
+  rand transaction tx;  //instantiating transaction class
+  mailbox gen2driv;  //instantiating mailbox and creating object
+  virtual intfc bus_tb; //instantiating interface using virtual keyword
   int tx_count;
   int uniq_id;
   logic [8-1:0] verif_data_q[$];
   logic [8-1:0] verif_wrdata;
   int i;
   event ended;
+
+  //constructor 
   function new(virtual intfc bus_tb, mailbox gen2driv);
-    this.bus_tb = bus_tb;
-    this.gen2driv = gen2driv;
+    this.bus_tb = bus_tb; /// Assign the passed interface to the member variable
+    this.gen2driv = gen2driv; /// Assign the passed mailbox to the member variable
   endfunction
   
   
   task main();
     $display("[ GENERATOR ] ****** GENERATOR STARTED ******");
- 
+
+    //creating a transaction
    tx = new();
 fork 
+   //write enable transaction generation
      for(int i=0;i <tx_count;i++) begin
        @(posedge bus_tb.wr_clk iff !bus_tb.full);
          bus_tb.wr_en = (i%1 == 0)? 1'b1 : 1'b0;
@@ -29,17 +33,16 @@ fork
           uniq_id ++;
           assert(tx.randomize());
           tx.uniq_id= uniq_id;
-          gen2driv.put(tx);
-         verif_data_q.push_front(bus_tb.data_in);
-           if(tx.uniq_id==1) begin
+          gen2driv.put(tx);    // putting randomized transaction in the mailbox 
+         verif_data_q.push_front(bus_tb.data_in); 
+          if(tx.uniq_id==1) begin
              $display("[ GENERATOR ]wdata  =%h, uniq_id=%d", tx.data_in,tx.uniq_id);
            end
         end
        
       end
-      for(int j=0;j<tx_count;j++) begin
-        
-        //@(posedge bus_tb.rclk)
+     //rd_en transaction generation
+     for(int j=0;j<tx_count;j++) begin
         @(posedge bus_tb.rd_clk iff !bus_tb.empty)
         bus_tb.rd_en = (j%3 == 0)? 1'b1 : 1'b0;
         if (bus_tb.rd_en) begin
@@ -48,7 +51,7 @@ fork
       end 
      join
     
-   -> ended;
+   -> ended; //trigerring the event handle
     $display("[ GENERATOR ] ****** GENERATOR ENDED ******");
   endtask
 endclass
