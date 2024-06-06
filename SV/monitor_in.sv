9d92d8f208f781ffae05cfb5271448ca6f36490c
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-05 18:37:04 -0700

    Update monitor_in.sv

--- SV/monitor_in.sv
+++ SV/monitor_in.sv
@@ -1,8 +1,10 @@
 class monitor_in;
-  virtual intfc vif;
-  mailbox mon_in2scb;
-  transaction tx=new();
+  virtual intfc vif; //virtual instance of interface
+  mailbox mon_in2scb; // instancing mailbox for monitor to scoreboard
+  transaction tx=new(); // creating a transaction
 int fd;
+
+  //constructor for assigning passing mailbox and interface to member variables  
   function new(virtual intfc vif, mailbox mon_in2scb);
     this.vif = vif;
     this.mon_in2scb = mon_in2scb;
@@ -21,7 +23,7 @@ int fd;
            tx.uniq_id =vif.uniq_id;
              if(vif.uniq_id ==1)
                $display("[ MONITOR]wdata =%h,uniq_id =%d \n",tx.data_in,tx.uniq_id);
-          mon_in2scb.put(tx);
+        mon_in2scb.put(tx); //putting the transsaction packet into the mailbox
          
           fd= $fopen("output1.txt","a");
         end
