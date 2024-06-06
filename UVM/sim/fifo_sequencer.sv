41a9f8050e5418bfa117304f0b007e9404828073
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-06-05 19:47:22 -0700

    Update fifo_sequencer.sv

--- UVM/sim/fifo_sequencer.sv
+++ UVM/sim/fifo_sequencer.sv
@@ -2,18 +2,21 @@ import uvm_pkg::*;
 `include "uvm_macros.svh"
 
 class sequencer extends uvm_sequencer #(fifo_seq_item);
-	`uvm_component_utils(sequencer) 
-	
+	`uvm_component_utils(sequencer) // registering sequencer component class to the factory
+
+	//constructor new 
 	function new (string name ="sequencer",uvm_component parent);
 		super.new(name,parent);
 		`uvm_info("SEQUENCER", "Inside Constructor!",UVM_LOW)
 	endfunction 
-	
+
+	//build_phase from uvm_phase
 	function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
 		`uvm_info("SEQUENCER", "Build Phase!",UVM_LOW)
 	endfunction
-	
+
+	//connect_phase from uvm_phase
 	function void connect_phase(uvm_phase phase);
 		super.connect_phase(phase);
 		`uvm_info("SEQUENCER", "Connect Phase!",UVM_LOW)
